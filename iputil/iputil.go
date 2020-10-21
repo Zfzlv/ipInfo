b3cf1d6b869c5f60b20cfc33e032d3964e39958f
li.jie <lijie@wuchaosuozhi.onaliyun.com>
2020-10-21 15:19:59 +0800

    add output use cn

--- iputil/iputil.go
+++ iputil/iputil.go
@@ -1,27 +1,29 @@
 package iputil
 
-import(
-	"net"
-	"github.com/Zfzlv/ipInfo/geo"
-	"github.com/Zfzlv/ipInfo/util"
+import (
 	"fmt"
+	"github.com/Zfzlv/ipInfo/geo"
+	"github.com/Zfzlv/ipInfo/reader"
+	"github.com/Zfzlv/ipInfo/util"
+	"net"
 	//"os"
-	"math/big"
 	"log"
+	"math/big"
 	//"time"
 	//"os/exec"
 	"path"
 	//"path/filepath"
-	"strings"
 	"runtime"
+	"strings"
 )
 
 type IpInfo struct {
-	IP         net.IP   `json:"ip"`   //origin ip
+	IP         net.IP   `json:"ip"`         //origin ip
 	IPDecimal  *big.Int `json:"ip_decimal"` //ip to bigInt
-	Country    string   `json:"country,omitempty"` 
-	CountryEU  *bool    `json:"country_eu,omitempty"` //is Eu Country？
+	Country    string   `json:"country,omitempty"`
+	CountryEU  *bool    `json:"country_eu,omitempty"`  //is Eu Country？
 	CountryISO string   `json:"country_iso,omitempty"` //ISO country code
+	Province   string   `json:"province,omitempty"`
 	City       string   `json:"city,omitempty"`
 	Hostname   string   `json:"hostname,omitempty"`
 	Latitude   float64  `json:"latitude,omitempty"`
@@ -30,33 +32,42 @@ type IpInfo struct {
 	ASNOrg     string   `json:"asn_org,omitempty"`
 }
 
-var(
+var (
 	Reader geo.Reader
-	err error
+	err    error
 )
 
-func init(){
+func init() {
 	log.Println("-init-geoReader-")
 	dir := getCurrentDirectory()
-	Reader, err = geo.Open(dir+"/GeoLite2-Country.mmdb",dir+"/GeoLite2-City.mmdb",dir+"/GeoLite2-ASN.mmdb")
+	Reader, err = geo.Open(dir+"/GeoLite2-Country.mmdb", dir+"/GeoLite2-City.mmdb", dir+"/GeoLite2-ASN.mmdb")
 	if err != nil {
-		log.Fatalln("-init-geoReader-err:-"+err.Error())
+		log.Fatalln("-init-geoReader-err:-" + err.Error())
+	}
+	region, err := reader.New(dir + "/Cnphy.db")
+	if err != nil {
+		log.Fatalln("-init-cnPhy-err:-" + err.Error())
+	} else {
+		defer region.Close()
+		//init to memory
+		ip, _ := region.MemorySearch("8.8.8.8")
+		log.Info(ip)
 	}
 }
 
 func getCurrentDirectory() string {
-    _, filename, _, ok := runtime.Caller(1)
-   var cwdPath string
-   if ok {
-     cwdPath = path.Join(path.Dir(filename), "")
-   }  else  {
-     cwdPath = "./"
-   }
-   cwdPath = strings.Replace(cwdPath,"iputil","data",-1)
-   return cwdPath
+	_, filename, _, ok := runtime.Caller(1)
+	var cwdPath string
+	if ok {
+		cwdPath = path.Join(path.Dir(filename), "")
+	} else {
+		cwdPath = "./"
+	}
+	cwdPath = strings.Replace(cwdPath, "iputil", "data", -1)
+	return cwdPath
 }
 
-func GetIpInfo(remoteIP string) (IpInfo,error){
+func GetIpInfo(remoteIP string, outputUseChinese bool) (IpInfo, error) {
 	if err != nil {
 		return IpInfo{}, err
 	}
@@ -66,9 +77,30 @@ func GetIpInfo(remoteIP string) (IpInfo,error){
 	city, _ := Reader.City(ip)
 	asn, _ := Reader.ASN(ip)
 	hostname, _ := util.LookupAddr(ip)
+	province := ""
 	var autonomousSystemNumber string
 	if asn.AutonomousSystemNumber > 0 {
 		autonomousSystemNumber = fmt.Sprintf("AS%d", asn.AutonomousSystemNumber)
+	}
+	if outputUseChinese {
+		cnIp, cnErr := region.MemorySearch(remoteIP)
+		if cnErr == nil && cnIp != "" {
+			str := strings.Split(cnIp, "|")
+			if len(str) > 5 {
+				if str[1] != "0" {
+					country.Name = str[1]
+				}
+				if str[3] != "0" {
+					province = str[3]
+				}
+				if str[4] != "0" {
+					city.Name = str[4]
+				}
+				if str[5] != "0" {
+					asn.AutonomousSystemOrganization = str[5]
+				}
+			}
+		}
 	}
 	return IpInfo{
 		IP:         ip,
@@ -76,6 +108,7 @@ func GetIpInfo(remoteIP string) (IpInfo,error){
 		Country:    country.Name,
 		CountryISO: country.ISO,
 		CountryEU:  country.IsEU,
+		Province:   province,
 		City:       city.Name,
 		Hostname:   hostname,
 		Latitude:   city.Latitude,
--- main_test.go
+++ main_test.go
@@ -1,24 +1,24 @@
 /**
 * @author zfzlv
 * 2019/09/26
-*/
+ */
 
 package main
 
-import(
+import (
 	"encoding/json"
 	"fmt"
 	"github.com/Zfzlv/ipInfo/iputil"
-	"time"
 	"testing"
+	"time"
 )
 
-func TestGetIpInfo(t *testing.T){
-	fmt.Println(time.Now(),"--test-ipInfo--")
-	r,e := iputil.GetIpInfo("114.84.151.36")
-	if e!=nil{
+func TestGetIpInfo(t *testing.T) {
+	fmt.Println(time.Now(), "--test-ipInfo--")
+	r, e := iputil.GetIpInfo("114.84.151.36", true)
+	if e != nil {
 		fmt.Println(e.Error())
-	}else{
+	} else {
 		b, _ := json.Marshal(r)
 		fmt.Println(string(b))
 	}
